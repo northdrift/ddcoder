043c7c53e24c7e6881bb993c298a92adb5661896
ddcoder <1094740701@qq.com>
2015-07-21 17:58:34 +0800

    添加获取随机字符串函数

--- CommonUtil.php
+++ CommonUtil.php
@@ -49,4 +49,20 @@ function downLoadFile($file)
         exit;
     }
 }
+
+/**
+ * 
+ * 产生随机字符串，不长于32位
+ * @param int $length
+ * @return 产生的随机字符串
+ */
+function getRandomStr($length = 32) 
+{
+    $chars = "abcdefghijklmnopqrstuvwxyz0123456789";  
+    $str ="";
+    for ( $i = 0; $i < $length; $i++ )  {  
+        $str .= substr($chars, mt_rand(0, strlen($chars)-1), 1);  
+    } 
+    return $str;
+}
 ?>
