8484ab209086f0f6ae5adc90ddaeadf61869fe59
ddcoder <1094740701@qq.com>
2015-08-04 15:25:10 +0800

    add Xss filter

---
+++ SecurityUtil.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ddcoder
+ * Date: 15-8-4
+ * Time: 下午1:19
+ */
+class SecurityUtil {
+
+    /**
+     * @blog http://www.phpddt.com
+     * @param $string
+     * @param $low 安全别级低
+     */
+    public static function cleanXss(&$string, $low = False)
+    {
+        if (! is_array ( $string ))
+        {
+            $string = trim ( $string );
+            $string = strip_tags ( $string );
+            $string = htmlspecialchars ( $string );
+            if ($low)
+            {
+                return True;
+            }
+            $string = str_replace ( array ('"', "\\", "'", "/", "..", "../", "./", "//" ), '', $string );
+            $no = '/%0[0-8bcef]/';
+            $string = preg_replace ( $no, '', $string );
+            $no = '/%1[0-9a-f]/';
+            $string = preg_replace ( $no, '', $string );
+            $no = '/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]+/S';
+            $string = preg_replace ( $no, '', $string );
+            return True;
+        }
+        $keys = array_keys ( $string );
+        foreach ( $keys as $key )
+        {
+            clean_xss ( $string [$key] );
+        }
+    }
+
+}
